1a62f372e03ba99f6490a683afb615bef7f0b4b3
allen3068 <allen.liu@technicolor.com>
2014-12-11 18:07:26 -0500

    remove one test

--- src/ExampleApp.cpp
+++ src/ExampleApp.cpp
@@ -14,7 +14,5 @@ int main()
     int z2 = Multiply::twoValues(x,y);
     printf("Multiply Result: %d\n", z2);
  
-    int z3 = Multiply::twoValues(x,y+2);
-    printf("Multiply Result: %d\n", z3);
     return 0;
 }
